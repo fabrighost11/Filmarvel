81355585fac95e7b49e1bbdd2a12775e3222391f
fabrighost11 <fabribianc1117@gmail.com>
2024-11-07 17:35:01 +0100

    Añadiendo ejemplos de config/web.php y db.php

---
+++ config/db.example.php
@@ -0,0 +1,9 @@
+<?php
+// db.example.php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=example_db',
+    'username' => 'example_user',
+    'password' => 'example_password',
+    'charset' => 'utf8',
+];
